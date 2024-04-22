f08b495bb2f8f203e398591c3033681287542448
Yor0071 <yorickboonstra@gmail.com>
2024-04-22 15:46:05 +0200

    Added window

--- LocatieBepalingInterface/src/main.java
+++ LocatieBepalingInterface/src/main.java
@@ -1,2 +1,18 @@
-public class main {
+import javax.swing.*;
+
+public class main extends JFrame
+{
+    public main()
+    {
+        setTitle("Locatie Bepaling");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args)
+    {
+        main frame = new main();
+        frame.setVisible(true);
+    }
 }
+
