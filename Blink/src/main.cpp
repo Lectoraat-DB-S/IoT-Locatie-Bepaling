b1d0bbdf2225eec06b8aa2d62feb8f1fb8051678
jonathan admiraal <s1184909@student.windesheim.nl>
2024-04-24 14:32:54 +0200

    via wifi data versturen

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -1,40 +1,77 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
 
-#include <Arduino.h>
+/* WiFi network name and password */
+// Your wirelless router ssid and password
+const char * ssid = "iotroam"; 
+const char * pwd = "VUrPivfEHF";
 
-#include "Wifi.h" 
+// IP address to send UDP data to.
+// it can be ip address of the server or 
+// a network broadcast address
+// here is broadcast address
+const char * udpAddress = "145.44.116.100"; // target pc ip
+const int udpPort = 8080; //port server
 
-#define LED 2
-
-const char* ssid = "yourNetworkName";
-const char* password = "yourNetworkPassword";
+//create UDP instance
+WiFiUDP udp;
 
 void setup(){
-    Serial.begin(115200);
-  pinMode(LED, OUTPUT);
-    delay(1000);
+  Serial.begin(115200); // to monitor activity
+  
+  //Connect to the WiFi network
+  WiFi.begin(ssid, pwd);
+  
 
-    WiFi.mode(WIFI_STA); //Optional
-    WiFi.begin(ssid, password);
-    Serial.println("\nConnecting");
-
-    while(WiFi.status() != WL_CONNECTED){
-        Serial.print(".");
-        delay(100);
-    }
-
-    Serial.println("\nConnected to the WiFi network");
-    Serial.print("Local ESP32 IP: ");
-    Serial.println(WiFi.localIP());
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  //This initializes udp and transfer buffer
+  udp.begin(udpPort);
 }
+long i=1;
+float iFloat;
+char iString[50] = "hello world";
+
+#define nBuffer 100
+char bufferData[nBuffer] = "hello world";
+void loop(){
+  //data dummy
+  i++;
+  iFloat = i/3.14;
+  
+
+  //----------------------------SEND
+  sprintf(bufferData, "%s;%d;%.3f",iString,i,iFloat);
+  Serial.println(bufferData);
+  
+  //send hello world to server
+  udp.beginPacket(udpAddress, udpPort);
+  udp.print(bufferData);
+  udp.endPacket();
+  memset(bufferData, 0, nBuffer);
+  //----------------------------SEND
 
 
+  //----------------------------RECEIVE
+  //processing incoming packet, must be called before reading the buffer
+  udp.parsePacket();
+  //receive response from server, it will be HELLO WORLD
+  if(udp.read(bufferData, 50) > 0){
+    Serial.print("Server to client: ");
+    Serial.println((char *)bufferData);
+  }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
-  //Serial.println("LED is on");
-  delay(1000);
-  digitalWrite(LED, LOW);
- //Serial.println("LED is off");
-  delay(1000);
+  //----------------------------SEND
+
+  
+  //Optional
+  delay(100);
 }
