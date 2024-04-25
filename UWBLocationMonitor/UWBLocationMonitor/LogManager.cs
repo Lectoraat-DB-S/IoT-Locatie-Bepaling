dc2ed7742be0cb850fe53f06d88832f2af7ab48a
Yor0071 <yorickboonstra@gmail.com>
2024-04-25 11:09:35 +0200

    Added Log panel

---
+++ UWBLocationMonitor/UWBLocationMonitor/LogManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UWBLocationMonitor
+{
+    public static class LogManager
+    {
+        private static readonly List<string> logMessages = new List<string>();
+        public static event Action<string> OnLogUpdate;
+
+        public static void Log(string message)
+        {
+            logMessages.Add(message);
+            OnLogUpdate?.Invoke(message);
+        }
+
+        public static IEnumerable<string> GetLogMessages()
+        {
+            return logMessages;
+        }
+    }
+}
