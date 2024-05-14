e36c67553f33b178cbc6c1218c2d2b1a66452c34
Yor0071 <yorickboonstra@gmail.com>
2024-05-14 13:26:21 +0200

    Update incoming data

--- UWBLocationMonitor/UWBLocationMonitor/NetworkConnection.cs
+++ UWBLocationMonitor/UWBLocationMonitor/NetworkConnection.cs
@@ -27,6 +27,7 @@ namespace UWBLocationMonitor
             isListening = true;
             Task.Run(() => ListenForMessages());
             LogManager.Log("Started connection");
+            LogManager.Log("Tag;X1;Y1;R1;X2;Y2;R2;X3;Y3;R3;");
         }
 
         private void ListenForMessages()
@@ -39,7 +40,7 @@ namespace UWBLocationMonitor
                     byte[] receivedBytes = udpClient.Receive(ref remoteEndPoint);
 
                     string receivedData = Encoding.UTF8.GetString(receivedBytes);
-                    LogManager.Log($"Received: {receivedData} from {remoteEndPoint}");
+                    //LogManager.Log(receivedData);
 
                     OnMessageReceived(receivedData);
                 }
@@ -62,8 +63,29 @@ namespace UWBLocationMonitor
 
         protected virtual void OnMessageReceived(string message)
         {
-            // Message handling
-            //LogManager.Log(message);
+            //Message handling
+            string[] parts = message.Split(";");
+            string tag = parts[0];
+
+            int[] numbers = new int[parts.Length - 2];
+            for (int i = 1; i < parts.Length - 1; i++)
+            {
+                numbers[i - 1] = int.Parse(parts[i]);
+            }
+
+            int X1 = numbers[0];
+            int Y1 = numbers[1];
+            int R1 = numbers[2];
+            int X2 = numbers[3];
+            int Y2 = numbers[4];
+            int R2 = numbers[5];
+            int X3 = numbers[6];
+            int Y3 = numbers[7];
+            int R3 = numbers[8];
+            
+            LogManager.Log(message);
+
+            TagManager.Instance.UpdateTagTrilateration(tag, X1, Y1, R1, X2, Y2, R2, X3, Y3, R3);
         }
     }
 }
