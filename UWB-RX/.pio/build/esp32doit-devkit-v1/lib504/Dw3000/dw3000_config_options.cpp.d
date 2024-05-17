cb1751baeb67fc7a95decca86f92d4464d0a1b4a
jonathan admiraal <s1184909@student.windesheim.nl>
2024-05-17 08:34:53 +0200

    added distance calculation

---
+++ UWB-RX/.pio/build/esp32doit-devkit-v1/lib504/Dw3000/dw3000_config_options.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/lib504/Dw3000/dw3000_config_options.cpp.o: \
+ lib/Dw3000/src/dw3000_config_options.cpp \
+ lib/Dw3000/src/dw3000_config_options.h \
+ lib/Dw3000/src/dw3000_device_api.h lib/Dw3000/src/dw3000_types.h
