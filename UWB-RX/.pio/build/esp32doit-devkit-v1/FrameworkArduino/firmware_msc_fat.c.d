f07a7ad58d1d98195880d76d67e4d2c701ff8bb2
jonathan admiraal <s1184909@student.windesheim.nl>
2024-05-23 10:39:25 +0200

    Anchor code added again

---
+++ UWB-RX/.pio/build/esp32doit-devkit-v1/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/Jonathan/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/Jonathan/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
