406d5dea20f656c29306ba59e3e0bd31274c591c
jonathan admiraal <s1184909@student.windesheim.nl>
2024-06-03 10:57:27 +0200

    Added Wifi via to the branch via the correct way

---
+++ UWB Tag/.pio/build/esp32doit-devkit-v1/lib60f/dw3000/dw3000_mac_802_15_4.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32doit-devkit-v1/lib60f/dw3000/dw3000_mac_802_15_4.cpp.o: \
+ lib/dw3000/dw3000_mac_802_15_4.cpp lib/dw3000/dw3000_device_api.h \
+ lib/dw3000/dw3000_types.h lib/dw3000/dw3000_mac_802_15_4.h \
+ lib/dw3000/dw3000_regs.h lib/dw3000/dw3000_vals.h \
+ lib/dw3000/dw3000_shared_defines.h
