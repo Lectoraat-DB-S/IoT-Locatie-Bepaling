5006d532a7bf3d940075acee70f894903d9e2fe0
JonathanAdmiraal <113357192+JonathanAdmiraal@users.noreply.github.com>
2024-06-14 14:23:17 +0200

    Delete UWB-RX directory

--- UWB-RX/.pio/build/esp32doit-devkit-v1/libc72/Dw3000/dw3000_config_options.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/libc72/Dw3000/dw3000_config_options.cpp.o: \
- lib/Dw3000/src/dw3000_config_options.cpp \
- lib/Dw3000/src/dw3000_config_options.h \
- lib/Dw3000/src/dw3000_device_api.h lib/Dw3000/src/dw3000_types.h
